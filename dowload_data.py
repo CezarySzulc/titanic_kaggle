43f3c20476ceb87cd7abded8b3d775ae3a83c00a
Cezary Szulc <cezary.szulc@gmail.com>
2018-01-14 17:07:04 +0100

    Add linear regression and xgboost, random forest still best

--- dowload_data.py
+++ dowload_data.py
@@ -2,21 +2,23 @@ from pandas import read_csv
 from sklearn.model_selection import train_test_split
 
 
-def create_data_frame(valid_size=0.2):
+def create_data_frame(need_valid_data=False, valid_size=0.2):
     """
     create train, valid and test sets,
     default valid set is 20% of train data
     """
+    # test data
+    test_data = read_csv('data/test.csv')
     # train data and valid data
     train_data = read_csv('data/train.csv')
     train_label = train_data['Survived']
     # train_data.drop(axis=1, labels='Survived', inplace=True)
-    train_data, valid_data, train_label, valid_label = train_test_split(
-        train_data, train_label, test_size=valid_size, random_state=43
-    )
-    # test data
-    test_data = read_csv('data/test.csv')
-    return train_data, train_label, valid_data, valid_label, test_data
+    if need_valid_data:
+        train_data, valid_data, train_label, valid_label = train_test_split(
+            train_data, train_label, test_size=valid_size, random_state=43
+        )
+        return train_data, train_label, valid_data, valid_label, test_data
+    return train_data, train_label, test_data
 
 
 if __name__ == '__main__':
--- pipeline.py
+++ pipeline.py
@@ -7,17 +7,27 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import classification_report
 import re
 import numpy as np
+# linear model
+from sklearn.linear_model import LogisticRegressionCV
+# xgb
+import xgboost as xgb
 
 
 class Model(object):
-    def __init__(self, train_data, train_label, valid_data, valid_label, test_data):
+    def __init__(self, need_valid_data=False):
+        if need_valid_data:
+            train_data, train_label, valid_data, valid_label, test_data = create_data_frame(True)
+            self.valid_data = valid_data
+            self.valid_label = valid_label
+            self.valid_data = self.clear_data(self.valid_data)
+        else:
+            train_data, train_label, test_data = create_data_frame()
+
+        self.need_valid_data = need_valid_data
         self.train_data = train_data
         self.train_label = train_label
-        self.valid_data = valid_data
-        self.valid_label = valid_label
         self.test_data = test_data
         self.train_data = self.clear_data(self.train_data)
-        self.valid_data = self.clear_data(self.valid_data)
         self.test_data = self.clear_data(self.test_data)
 
     def clear_data(self, data):
@@ -25,7 +35,7 @@ class Model(object):
         data['Age'] = data['Age'].fillna(data['Age'].median())
         data['Fare'] = data['Fare'].fillna(data['Fare'].median())
         data['Age'] = qcut(data.Age, q=6, labels=False)
-        data['Fare'] = qcut(data.Fare, q=6, labels=False)
+        data['Fare'] = qcut(data.Fare, q=10, labels=False)
         # change 'Sex' columns into int type
         data = get_dummies(data, columns=['Sex'], drop_first=True)
         data = get_dummies(data, columns=['Embarked'], drop_first=True)
@@ -59,30 +69,40 @@ class Model(object):
     def pipeline(self):
         columns = self.test_data.columns.tolist()
         columns.remove('PassengerId')
-        model = RandomForestClassifier(
-            # n_estimators=100,
-            # max_depth=10,
-            # n_jobs=-1,
-            # random_state=43
-        )
-        param_grid = {
-            'n_estimators': np.arange(5, 100, 5),
-            'max_depth': np.arange(1, 20)
-        }
-        model = GridSearchCV(model, param_grid=param_grid, cv=5)
+        # xgboost
+        model = xgb.XGBClassifier()
+        # linear regression
+        # model = LogisticRegressionCV(cv=5)
+        # Randorm forest tree clf
+        # model = RandomForestClassifier(
+        #     random_state=43,
+        #     n_jobs=-1,
+        #     max_depth=6,
+        #     n_estimators=40,
+        #
+        # )
+        # param_grid = {
+        #     # 'n_estimators': np.arange(5, 100, 5),
+        #     # 'max_depth': np.arange(1, 12),
+        #     'min_samples_split': np.arange(2, 8)
+        # }
+        # model = GridSearchCV(model, param_grid=param_grid, cv=5)
         model.fit(self.train_data[columns], self.train_label)
-        # print(self.test_data.Age)
-        print(model.score(self.valid_data[columns], self.valid_label))
-        print(classification_report(model.predict(self.valid_data[columns]), self.valid_label))
+        # print('Model best params: {}'.format(model.best_params_))
+        # print('Model best score: {}'.format(model.best_score_))
+        # print(model.best_estimator_)
+        # print(model.scores_)
+        if self.need_valid_data:
+            print(model.score(self.valid_data[columns], self.valid_label))
+            print(classification_report(model.predict(self.valid_data[columns]), self.valid_label))
         self.test_data['Survived'] = model.predict(self.test_data[columns])
 
     def save_test_output(self, name):
         self.test_data[['PassengerId', 'Survived']].to_csv('data/{}.csv'.format(name), index=False)
 
 
-_train_data, _train_label, _valid_data, _valid_label, _test_data = create_data_frame()
-my_model = Model(_train_data, _train_label, _valid_data, _valid_label, _test_data)
+my_model = Model()
 my_model.pipeline()
-my_model.save_test_output('random_forest')
+my_model.save_test_output('xgboost')
 # pipeline = Pipeline()
 # pipeline = Pipeline()
